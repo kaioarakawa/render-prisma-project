d21616632a2a58b515a5efb6f3170a176cc602f7
KAIO TAKESHI ARAKAWA DOS SANTOS <kaio.santos@prof.sc.senac.br>
2025-09-23 20:48:21 -0300

    chore: add conection to render

--- index.js
+++ index.js
@@ -13,6 +13,17 @@ app.post("/user", async (req, res) => {
     res.json(user);
 });
 
+app.get("/user/:id", async (req, res) => {
+    const { id } = req.params;
+    const user = await prisma.user.findUnique({ where: { id: Number(id) } })
+
+    if(!user){
+        return res.status(404).json({ error: "Usuario não encontrado"});
+    }
+
+    res.json(user);
+});
+
 app.get("/users", async (req, res) => {
     const users = await prisma.user.findMany();
 
